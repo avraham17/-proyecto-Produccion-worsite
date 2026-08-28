60fec9f435582c3dc6ca449f2866a6bee5040bc4
unknown <avrahampg1999@hotmail.com>
2026-08-28 16:14:28 -0500

    act

--- worksite/src/main/java/edu/co/sena/worksite/services/EstadisticasService.java
+++ worksite/src/main/java/edu/co/sena/worksite/services/EstadisticasService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EstadisticasService {
+public class    EstadisticasService {
 
     @Autowired
     private ResgistroUsuarioRepository usuarioRepository;
